1dfda3c2025a34635f5334680e7a2c13ed7380fe
Md Asaduzzaman <asad@aadibd.com>
2022-05-20 13:16:30 +0600

    install beautifulsoup4 for scrap image

--- apps/product/utils/scrap_products.py
+++ apps/product/utils/scrap_products.py
@@ -2,3 +2,7 @@ URLS = [
     'https://www.aarong.com/catalog/product/view/id/656192/s/yellow-half-silk-jamdani-saree/category/233/',
     'https://www.aarong.com/men/panjabi/black-brush-painted-and-printed-endi-silk-panjabi-15f220220047.html'
 ]
+
+
+def scrap_product_images():
+    pass
