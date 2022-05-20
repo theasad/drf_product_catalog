c9be9a6d11d821bdab7c9f60466549f4ad9e0326
Md Asaduzzaman <asad@aadibd.com>
2022-05-20 12:52:52 +0600

    add product link from aarong.com

---
+++ apps/product/utils/scrap_products.py
@@ -0,0 +1,4 @@
+URLS = [
+    'https://www.aarong.com/catalog/product/view/id/656192/s/yellow-half-silk-jamdani-saree/category/233/',
+    'https://www.aarong.com/men/panjabi/black-brush-painted-and-printed-endi-silk-panjabi-15f220220047.html'
+]
